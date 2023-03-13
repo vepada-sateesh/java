4e050d0a4627567c67925d8036f57dc5a48d669f
vepada-sateesh <sateeshvepadas@gmail.com>
2023-03-13 15:02:23 +0530

    added

--- Hello.java
+++ Hello.java
@@ -1,18 +1,28 @@
-class Students {
-    int rollno;
-    String name;
-    int marks;
-    
 
-}
-    class Hello
-    {
-        public static void main(String a[])
-        {
-            StringBuffer name = new StringBuffer("sateesh");
-            name.append("vepada");//a lot of method to change string;
-            System.out.println(name);
-            System.out.println(name.capacity());
-        }
-        
+class A extends Object {
+    public A() {
+        System.out.println("defa A");
     }
+
+    public A(int m) {
+        System.out.println("int param A");
+    }
+}
+
+class B extends A {
+    public B() {
+        super();
+        System.out.println("int b");
+    }
+
+    public B(int n) {
+        this();
+        System.out.println("int b int");
+    }
+}
+
+class Demo {
+    public static void main(String[] args) {
+        B s1 = new B(5);
+    }
+}
